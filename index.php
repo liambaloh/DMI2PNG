32a486857f8c8008527ba05192723468f0061163
Xkeeper <xkeeper@gmail.com>
2019-03-25 14:15:53 -0700

    Split stuff to include file

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 <?php
 
-
-    require_once "lib/GifCreator.php";
-    require_once "lib/PNGMetadataExtractor.php";
-
-    function startsWith($haystack, $needle) {
-        // search backwards starting from haystack length characters from the end
-        return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
-    }
-    function endsWith($haystack, $needle) {
-        // search forward starting from end minus needle length characters
-        return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
-    }
-
+    require_once "libs.php";
 
     $spritesPerLoad = 1000;
     $dmiFiles = Array();
